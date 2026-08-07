f6a2f98da36520f23a64c7e24b1fe13389de6c0e
mdujjalhossain <mdujjalhossain.cse@gmail.com>
2026-08-07 17:40:22 +0600

    for loop and while loop in python

---
+++ for-loop.py
@@ -0,0 +1,35 @@
+# for i in range(10):
+#     print(i)
+
+# a = list(range(0, 10, 2))   # range function (start, end, step)
+# print(a)
+
+# b = list(range(10, 0, -2))   # list function to list data in array format.
+# print('reverse list: ' ,b)
+
+# a = 'hello world'
+# for words in a:
+#     print(words)
+
+# bag = ['alu', 'potol', 'Kumra', 20, 15, 45.469]
+# for item in bag:
+#     print(item)
+#     print(type(item))
+
+# num = [12, 37, 10, 34, 18, 8, 90, 7]
+# for i in num:
+#     if i <= 20:
+#         print(i)
+
+# problem-> divisible by 3 and 5 and print
+# for i in range(20):
+#     if (i % 3 == 0 and i % 5 == 0):
+#         print(i)
+
+#  sum 1 to 10 
+sum = 0
+for i in range(1, 11):
+    sum = sum + i
+print(sum)
+    
+    
