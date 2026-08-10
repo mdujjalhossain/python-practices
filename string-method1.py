8c1a06cd2f1b0fce4ef64fa7071efaddbff67e69
mdujjalhossain <mdujjalhossain.cse@gmail.com>
2026-08-10 16:18:34 +0600

    string methods

---
+++ string-method1.py
@@ -0,0 +1,26 @@
+# lower(), converts characters into lowercase
+a = 'Hello World Python'
+b = 'I Am Testing You'
+print('lower: ',a.lower())
+
+# upper(), converts character into uppercase
+print('upper: ' ,a.upper())
+
+# title(), converts string to title case
+print( 'title: ',a.title())
+
+# isupper()
+print( 'isupper? ',a.isupper())
+
+# islower()
+print( 'islower? ',a.islower())
+
+# istitle()
+print( 'istitle? ',b.istitle())
+# print('hello'.istitle())
+
+# isalpha()
+x = 'hello123'
+print('isalpha? ', x.isalpha())
+print('isalpha? ', 'Hello'.isalpha())
+
---
+++ string-method2.py
@@ -0,0 +1,38 @@
+# capitalize()
+a = 'hello world python'
+print('capitalized: ' ,a.capitalize())
+
+# title()
+print('titled: ', a.title())
+
+# swapcase()
+b = 'heLLo WorlD'
+print('swapcase: ', b.swapcase())
+
+# casefold()  # uses like lowercase but more power full for special word: -> groß
+c = 'groß'
+print('casefold: ', c.casefold())
+
+# replace()     # we can replace(old, new, count) either by indexing or exact value
+d = 'Hellp'
+print('replace: ',d.replace(d[-1], 'o'))
+print('another replace: ',d.replace('l', 'k', 2))  
+
+# count()
+print('count: ',d.count('l'))
+
+# isdigit()
+e = 'H10' 
+
+f = '10'
+print('isdigit? ',e.isdigit())
+print('isdigit? ',f.isdigit())
+
+# join()    join() --> list to sting  &  split() --> string to list
+g = ['h','e','l','l','o' ]
+print(''.join(g))
+h= ['1','2', '3']
+print(''.join(h))
+print(type(g))
+
+
